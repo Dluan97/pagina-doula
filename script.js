e744bd75219b0f9f78940217e4fca020221daccb
Dluan97 <dluanlopes9@gmail.com>
2025-10-23 19:04:53 -0300

    primeiro commit

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-// Detecta quando o usuário chega na parte de baixo (ex: seção Serviços)
-window.addEventListener("scroll", () => {
-  const header = document.getElementById("header");
-  const servicos = document.getElementById("servicos");
-  const scrollPosition = window.scrollY;
 
-  // Quando o scroll ultrapassa o topo da seção de serviços
-  if (scrollPosition > servicos.offsetTop - 100) {
-    header.classList.add("transparent");
-  } else {
-    header.classList.remove("transparent");
-  }
-});
